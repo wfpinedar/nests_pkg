985710aee4d66633672be7ee98ef6f91b1e81314
Fernado Pineda <wfpinedar@gmail.com>
2020-03-17 14:53:06 -0500

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,7 +128,8 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
-          point['Description']);
+          "<b>" point['Description'] '</b><br>' + 
+          "<b>" point['Latitude'] + "," + point['Longitud'] + '</b>');
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
